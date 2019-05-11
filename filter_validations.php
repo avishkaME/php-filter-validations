f180470a7d50b3773aa6cc2a0651be9861d1c489
avishkaME <ameddegoda143@gmail.com>
2019-05-11 12:19:30 +0530

    check for posted data in a form

---
+++ filter_validations.php
@@ -0,0 +1,23 @@
+<?php
+  //check for posted data
+  if (filter_has_var(INPUT_POST, 'data')) {
+    echo "data found";
+  }else{
+    echo "not found data";
+  }
+
+  // email validation
+  /*if (filter_has_var(INPUT_POST, 'data')) {
+    if (filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)) {
+      echo "e mail is valid";
+    }else {
+      echo "e mail is not valid";
+    }
+  }*/
+ ?>
+
+
+<form class="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+  <input type="text" name="data" value="">
+  <button type="submit" name="button">Submit</button>
+</form>
