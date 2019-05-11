d47ec47f4a57a742315d45eb1cdfb2c57598c1ee
avishkaME <ameddegoda143@gmail.com>
2019-05-11 12:20:43 +0530

    e-mail validation

--- filter_validations.php
+++ filter_validations.php
@@ -1,19 +1,19 @@
 <?php
   //check for posted data
-  if (filter_has_var(INPUT_POST, 'data')) {
+  /*if (filter_has_var(INPUT_POST, 'data')) {
     echo "data found";
   }else{
     echo "not found data";
-  }
+  }*/
 
   // email validation
-  /*if (filter_has_var(INPUT_POST, 'data')) {
+  if (filter_has_var(INPUT_POST, 'data')) {
     if (filter_input(INPUT_POST, 'data', FILTER_VALIDATE_EMAIL)) {
       echo "e mail is valid";
     }else {
       echo "e mail is not valid";
     }
-  }*/
+  }
  ?>
 
 
